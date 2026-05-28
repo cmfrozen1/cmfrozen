a88a4b10afc6e90f439a52a047125ef53f7f1723
cmfrozen1 <aatkcmf@gmail.com>
2026-05-28 15:07:50 +0700

    superbase

---
+++ public/React CRUD/crudreact/src/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
